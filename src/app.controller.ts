b3f2b3f0090a4ce4b11c415b4bc7cdb004c855f9
Vinicius Ribeiro <vayne99@gmail.com>
2023-04-10 07:29:04 -0300

    feat: enable cors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,6 @@ export class AppController {
 
   @Get()
   getHello() {
-    return this.appService.getHello();
+    return this.appService.healthcheck();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import data from '../data/ManCity_Arsenal_events.json';
 
 @Injectable()
 export class AppService {
-  getHello() {
-    const eventTypes = data
-      .map((event) => event.type)
-      .filter(
-        (value, index, self) =>
-          index === self.findIndex((t) => t.name === value.name),
-      );
-    return data[0];
+  healthcheck() {
+    return {
+      message: 'connected',
+    };
   }
 }
