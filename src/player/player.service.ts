c0444ba35c5ad779bfd85ec5762110b6e07a3f1d
Vinicius Ribeiro <vayne99@gmail.com>
2023-04-17 07:14:56 -0300

    feat: list all player brings stats and events info

--- src/player/player.service.ts
+++ src/player/player.service.ts
@@ -6,7 +6,45 @@ export class PlayerService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async listAllPlayers() {
-    return await this.prismaService.player.findMany();
+    return await this.prismaService.player.findMany({
+      select: {
+        id: true,
+        name: true,
+        height: true,
+        weigth: true,
+        events: {
+          select: {
+            type: true,
+            match: {
+              select: {
+                date: true,
+              },
+            },
+          },
+        },
+        playerStats: {
+          select: {
+            walking: true,
+            jogging: true,
+            running: true,
+            high_speed_running: true,
+            sprinting: true,
+            distance: true,
+            average_speed: true,
+            max_speed: true,
+            minute_from: true,
+            minute_to: true,
+            match: {
+              select: {
+                opponent: true,
+                date: true,
+              },
+            },
+          },
+        },
+        observations: true,
+      },
+    });
   }
 
   async findPlayerById(id: number) {
