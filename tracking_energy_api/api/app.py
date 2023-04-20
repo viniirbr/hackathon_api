f03d295d3af7ceffd40d03122dc8cc676d93f8b9
Javi <javiecija96@gmail.com>
2023-04-20 18:26:02 +0200

    prediction

--- tracking_energy_api/api/app.py
+++ tracking_energy_api/api/app.py
@@ -1,25 +1,46 @@
 import json
 from flask import Flask, request, jsonify
 from datetime import datetime
-from energy_calculator import energy_level_of_player_in_game
+from energy_calculator import energy_level_of_player_in_game, predict_time_to_threshold
 from flask_pymongo import PyMongo
 from mongo_connection import uri
+from math import floor
 
+from asgiref.sync import sync_to_async
 
 
 app = Flask(__name__)
 client = PyMongo(app, uri=uri)
+@app.route('/')
+def status():
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
-@app.route('/', methods = ["GET"])
-def energy_calculation():
+@app.route('/energy', methods = ["GET"])
+async def energy_calculation():
+    ''' returns enery level as a number between 0 and 1 if the information is found, otherwise sends "No match or player found" message'''
     ssiId = request.args.get('ssiId')
     date = request.args.get('date') or datetime.strftime(datetime.now().date(), "%Y-%m-%d")
     db = client.db
-    final_energy_level = energy_level_of_player_in_game(db, ssiId, date, recovery_factor=0.2, calory_start=2000)
-    # returns enery level as a number between 0 and 1 if the information is found, otherwise sends "No match or player found" message
-    return jsonify(final_energy_level)
+    final_energy_level = await sync_to_async(energy_level_of_player_in_game)(db, ssiId, date, recovery_factor=0.2, calory_start=2000)
+    response = final_energy_level or "No player or match found"
+    return jsonify(response)
 
-@app.route('/', methods = ["POST"])
+@app.route('/energy/predict', methods = ["GET"])
+async def energy_prediction():
+    ''' returns an estimated amount of minutes to reach a certain energy level from last data'''
+    ssiId = request.args.get('ssiId')
+    date = request.args.get('date') or datetime.strftime(datetime.now().date(), "%Y-%m-%d")
+    try:
+        threshold = float(request.args.get('threshold'))
+    except:
+        threshold = 0.5
+    db = client.db
+    energy_levels = await sync_to_async(energy_level_of_player_in_game)(db, ssiId, date, recovery_factor=0.2, calory_start=2000, cum = True)
+    prediction = {"minutes" : floor(predict_time_to_threshold(energy_levels, threshold))} if energy_levels is not None else "No player or match found"
+    return jsonify(prediction)
+
+
+@app.route('/tracking', methods = ["POST"])
 def add_tracking_data():
     content = request.get_json()
     #date = content.get("date") consider this in case we want to update a past game, otherwise add it to today
--- tracking_energy_api/api/energy_calculator.py
+++ tracking_energy_api/api/energy_calculator.py
@@ -5,23 +5,27 @@ from datetime import datetime
 def mets(speed, weight, seconds = 0.04):
     # Intended to receive arrays of speeds 
     # Formula of mets(speed, weight, seconds=60) in miles/pounds https://tbonesbaseball.com/calculating-your-mets-for-running/#:~:text=The%20formula%20for%20calculating%20your,amount%20of%20energy%20at%20rest.
+    #combined with this info because the above formula doesn't make sense (more mets at slower paces) https://www.livestrong.com/article/49231-mets-treadmill/
     # Recall that mets are a unit per minute
     #1 mile = 1609,34 meters
     #1 minute = 60 seconds
     #1 kg = 2,20462 pounds
     # potential research https://exrx.net/Aerobic/WalkCalExp and calculator https://exrx.net/Calculators/WalkRunMETs
-    metersecond_to_milesminute = 1609.34 / 60 
+    # https://tbonesbaseball.com/speed-a-risk-factor-for-developing-mets/ from here I deduced each extra 0.5mph adds extra 0.75 mets, so I just need to convert units to know how many mets adds each extra meter per second
+    #metersecond_to_milesminute = 60 / 1609.34 
+    mph_to_ms = 1609.34 / 3600
     kg_to_pound = 2.20462
-    METs = (0.0175 * metersecond_to_milesminute * speed) + (0.0001 * kg_to_pound * weight) 
+    METs = (1.5 * mph_to_ms * speed) + (1/60 * kg_to_pound * weight) 
     interval_mets = seconds * METs / 60
     return interval_mets
 
-def calories_burnt(mets, weight):
+def calories_burnt(mets, weight, cum = False):
     # Intended to receive an array of mets
     # formula https://marathonhandbook.com/what-are-mets/
     # time could be introduced here instead of in mets https://www.cmsfitnesscourses.co.uk/blog/using-mets-to-calculate-calories-burned/
-    calories = (mets * 3.5 * weight ) / 200
-    return np.sum(calories)
+    # 1 met corresponds to burning 1 calory after 1 hour (60 minutes, since mets are a unit per minute) for each kilogram
+    calories = (mets * weight ) / 60
+    return np.cumsum(calories) if cum else np.sum(calories)
 
 def energy_level(calories_burnt, calory_baseline = 2400, calory_start = 2000, recent_activity_factor = 1, recovery_factor = 0, previous_games_factor = 1):
     #default energy based on https://www.healthychildren.org/English/health-issues/injuries-emergencies/sports-injuries/Pages/Female-Athlete-Triad.aspx#:~:text=Most%20female%20athletes%20need%20a,help%20the%20athlete%20perform%20better!
@@ -34,7 +38,7 @@ def energy_level(calories_burnt, calory_baseline = 2400, calory_start = 2000, re
     #    warnings.warn("Warning: run out of energy")
     return energy_level
 
-def energy_level_of_player_in_game(db, ssiId, date, calory_baseline = 2400, calory_start = 2000, recent_activity_factor = 1, recovery_factor = 0, previous_games_factor = 1):
+def energy_level_of_player_in_game(db, ssiId, date, calory_baseline = 2400, calory_start = 2000, recent_activity_factor = 1, recovery_factor = 0, previous_games_factor = 1, cum = False):
     col = db.tracking
     date = datetime.strptime(date,"%Y-%m-%d")
     my_doc = col.find_one({"match_date" : date, "ssiId": ssiId},{"_id":0, "speeds":1})
@@ -45,15 +49,22 @@ def energy_level_of_player_in_game(db, ssiId, date, calory_baseline = 2400, calo
         speeds1, speeds2 = np.array([speed["speed"] for speed in speeds1]), np.array([speed["speed"] for speed in speeds2])
 
         mets1 = mets(speeds1, weight)
-        calories1 = calories_burnt(mets1, weight)
+        calories1 = calories_burnt(mets1, weight, cum = cum)
         energy1 = energy_level(calories1, calory_start=calory_start, calory_baseline=calory_baseline)
         mets2 = mets(speeds2, weight)
         calories2 = calories_burnt(mets2, weight)
         energy2 = energy_level(calories2, recent_activity_factor=energy1, recovery_factor=recovery_factor, calory_start=calory_start, calory_baseline=calory_baseline)
-
         return energy2
     else:
-        return "No match or player found"
+        return None
+
+def predict_time_to_threshold(energy_level, energy_threshold):
+    measures = len(energy_level)
+    interval = np.arange(0,measures)
+    m,c = np.polyfit(interval, energy_level, 1) #y = mx+c
+    time = (energy_threshold - c)/m
+    minutes_remaining = (time - measures) * 0.04 / 60
+    return minutes_remaining
 
 
 # wrap in a class that contains the baseline, start and db, 
